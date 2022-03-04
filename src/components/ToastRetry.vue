1dfab8442619a338bde6ad5c4b0b5d55f9bfde0c
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-03-04 18:40:32 +0000

    Now retry toast dosen't need props it receives it's content by slot and sends events when the user interacts with it's buttons

--- src/components/ToastRetry.vue
+++ src/components/ToastRetry.vue
@@ -2,7 +2,7 @@
   <div class="position-absolute bottom-0 start-50 translate-middle-x mb-4">
     <div ref="toastElem" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="toast-body">
-        {{ innerText }}
+        <slot></slot>
         <div class="mt-2 pt-2 border-top">
           <button type="button" class="btn btn-secondary btn-sm m-1" data-bs-dismiss="toast">Close</button>
           <button type="button" class="btn btn-primary btn-sm mr-1" @click.stop="retryClickHandler">Retry</button>
@@ -17,8 +17,7 @@ import { defineComponent, ref, onBeforeUnmount, onMounted } from 'vue'
 import { Toast } from 'bootstrap'
 
 export default defineComponent({
-  props: ['innerText', 'retryAction'],
-  setup (props) {
+  setup (_, ctx) {
     const toastElem = ref(null)
     const show = () => {
       if (toastElem.value) {
@@ -26,7 +25,7 @@ export default defineComponent({
       }
     }
     const retryClickHandler = () => {
-      props.retryAction()
+      ctx.emit('retryClick')
       if (toastElem.value) {
         Toast.getInstance(toastElem.value)?.hide()
       }
