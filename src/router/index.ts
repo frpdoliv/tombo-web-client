a3085f4152c14943b8c9c8576bf40b5c006b6c8c
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-03-04 12:12:01 +0000

    Created middleware to check if user is authenticated

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,27 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import GettingStartedView from '../views/GettingStartedView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import SignUpView from '../views/auth/SignUpView.vue'
+import { isAuthenticated } from '@/code/authentication-status-manager'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    middleware?: Set<string>
+  }
+}
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/',
+    redirect: { name: 'dashboard' }
+  },
+  {
     path: '/getting-started',
     name: 'getting-started',
-    component: GettingStartedView
+    component: GettingStartedView,
+    meta: {
+      middleware: new Set(['guest'])
+    }
   },
   {
     path: '/dashboard',
@@ -18,13 +31,22 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/sign-up',
     name: 'sign-up',
-    component: SignUpView
+    component: SignUpView,
+    meta: {
+      middleware: new Set(['guest'])
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to) => {
+  if (!isAuthenticated() && !to.meta?.middleware?.has('guest') && to.name !== 'getting-started') {
+    return { name: 'getting-started' }
+  }
 })
 
 router.beforeEach((to) => {
